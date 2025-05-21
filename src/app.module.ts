480b1cfa087004881fd910a1141b5e0950dd0bd4
Jagadeep <jagadeep@mantracollab.com>
2025-05-21 09:10:11 +0530

    removed customer and post module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,8 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomersModule } from './customers/customers.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { PostsModule } from './posts/posts.module';
 import configuration from './config/configuration';
 import { MailModule } from './mail/mail.module';
 import { CommonModule } from './common/common.module';
@@ -34,9 +32,7 @@ import { CommonModule } from './common/common.module';
       },
     ]),
     DatabaseModule,
-    CustomersModule,
     AuthModule,
-    PostsModule,
     MailModule,
     CommonModule,
   ],
