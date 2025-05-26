f3da59d378cc7ddd3ce7847897d1feeb919b6876
Jagadeep <jagadeep@mantracollab.com>
2025-05-26 14:14:52 +0530

    add frontend url on cors

--- src/main.ts
+++ src/main.ts
@@ -25,8 +25,15 @@ async function bootstrap() {
     // Enable global exception filter
     app.useGlobalFilters(new AllExceptionsFilter());
 
+    // Enable CORS with specific settings for Swagger UI
+    // Enable CORS with secure cookie settings
+    // const corsOrigin =
+    //   process.env.NODE_ENV === 'production'
+    //     ? process.env.CORS_ORIGIN || 'https://your-frontend-domain.com'
+    //   : true; // Allow all origins in development
+
     app.enableCors({
-      origin: true,
+      origin: process.env.FRONTEND_URL,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
       credentials: true, // Required for cookies
